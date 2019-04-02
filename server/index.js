c5a6bb1ab6cb0b6b9d61aa444a3fdce35c3eb4e0
Muhammad Jamaluddin <mohamed.eldeen.399@gmail.com>
2019-04-02 20:12:59 +0200

    Implement the patch route

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const { review } = require('../db/models.js');
 
 const port = 3003;
@@ -13,6 +15,23 @@ app.get('/reviews/:id', (req, res) => {
   review.find({ id: req.params.id }).exec()
     .then((results) => {
       res.send(results);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+const jsonParser = bodyParser.json();
+app.patch('/review', jsonParser, (req, res) => {
+  console.log(req.body.reviewId);
+  console.log(req.body.likes_count);
+  review.findOneAndUpdate(
+    { _id: req.body.reviewId },
+    { likes_count: Number(req.body.likes_count) + 1 },
+    { useFindAndModify: false },
+  ).exec()
+    .then(() => {
+      res.end();
     })
     .catch((err) => {
       console.log(err);
