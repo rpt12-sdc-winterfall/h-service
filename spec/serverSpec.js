f2a95a375950f37660bacb74c9fc22d2e2802856
Muhammad Jamaluddin <mohamed.eldeen.399@gmail.com>
2019-04-09 09:58:32 +0200

    Refactor integration tests
    - I used `axios` instead of `request` because of its promise-based
    convenience.
    - I refactored the get request to be within the `before` function
    to prevent duplication & make the test cases run faster.
    - I added an additional test case for the `/review PATCH` route.

--- spec/serverSpec.js
+++ spec/serverSpec.js
@@ -2,28 +2,52 @@
  * for these tests to pass. */
 
 const { expect } = require('chai');
-const request = require('request');
+const axios = require('axios');
 
 describe('the server', () => {
-  it('the data/documents are in the right format & adhere to the schema', (done) => {
-    request('http://localhost:3003/reviews/20', (error, response, body) => {
-      const result = JSON.parse(body);
-      const review = result[0];
+  let result;
 
-      expect(review).to.have.all.keys('_id', 'id', 'image_url', 'reviewer_name', 'star_rate', 'review_date', 'review_description', 'likes_count');
-      done();
-    });
+  // runs before all tests in this block
+  before((done) => {
+    axios('http://localhost:3003/reviews/20')
+      .then((response) => {
+        result = response.data;
+        done();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   });
 
-  it('the reviews are the ones associated to the book through its id', (done) => {
-    request('http://localhost:3003/reviews/20', (error, response, body) => {
-      const result = JSON.parse(body);
+  it('the data/documents are in the right format & adhere to the schema', () => {
+    const review = result[0];
 
-      for (let i = 0; i < result.length; i += 1) {
-        expect(result[i].id).to.equal(20);
-      }
+    expect(review).to.have.all.keys('_id', 'id', 'image_url', 'reviewer_name', 'star_rate', 'review_date', 'review_description', 'likes_count');
+  });
 
-      done();
-    });
+  it('the reviews are the ones associated to the book through its id', () => {
+    for (let i = 0; i < result.length; i += 1) {
+      expect(result[i].id).to.equal(20);
+    }
+  });
+
+  it('should update the likes count for the specified review', (done) => {
+    const oldLikesCount = result[0].likes_count;
+
+    const review = {
+      reviewId: result[0]._id,
+      likes_count: oldLikesCount,
+    };
+
+    axios.patch('http://localhost:3003/review', review)
+      .then(() => axios.get('http://localhost:3003/reviews/20'))
+      .then(response => response.data[0])
+      .then((updatedReview) => {
+        expect(updatedReview.likes_count).to.equal(oldLikesCount + 1);
+        done();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   });
 });
